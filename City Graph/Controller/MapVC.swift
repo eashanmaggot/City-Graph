8f982481c5be9d9dc62238112a13d972317d6e69
Eashan <eashan@Eashans-Mac-mini.local>
2017-10-06 23:10:46 +0600

    Initial Commit

---
+++ City Graph/Controller/MapVC.swift
@@ -0,0 +1,16 @@
+//
+//  MapVC.swift
+//  City Graph
+//
+//  Created by Eashan on 6/10/17.
+//  Copyright © 2017 The Dev Dude. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
